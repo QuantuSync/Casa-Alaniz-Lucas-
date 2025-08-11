e06ee3cde1b5b932e487b5302b17c685e79f6856
QuantuSync <lucasalanizpintos@gmail.com>
2025-08-11 15:40:43 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,14 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-    // Optimización para producción
-    ...(process.env.NODE_ENV === 'production' && {
-      cssnano: {
-        preset: ['default', {
-          discardComments: { removeAll: true },
-          normalizeWhitespace: false,
-        }]
-      }
-    })
   }
 };

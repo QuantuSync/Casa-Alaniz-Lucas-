2f14e1898b86d3bf5f022c02442ee001990361e8
QuantuSync <lucasalanizpintos@gmail.com>
2025-08-14 21:11:19 +0200

    Update AppRouter.tsx

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -4,20 +4,34 @@ import Historia from "../pages/Historia";
 import Simbolos from "../pages/Simbolos";
 import Login from "../pages/Login";
 import Miembros from "../pages/Miembros";
+import Legado from "../pages/Legado";
+import Documentos from "../pages/Documentos";
+import Condecoraciones from "../pages/Condecoraciones";
+import SedeElectronica from "../pages/SedeElectronica";
+import Contacto from "../pages/Contacto";
+import ChatFamiliar from "../pages/ChatFamiliar";
+import ChatAdmin from "../pages/ChatAdmin";
 
 const AppRouter = () => {
   const isAuthenticated = localStorage.getItem("auth") === "true";
-
+  
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/historia" element={<Historia />} />
       <Route path="/simbolos" element={<Simbolos />} />
+      <Route path="/legado" element={<Legado />} />
+      <Route path="/documentos" element={<Documentos />} />
+      <Route path="/condecoraciones" element={<Condecoraciones />} />
+      <Route path="/sede-electronica" element={<SedeElectronica />} />
+      <Route path="/contacto" element={<Contacto />} />
       <Route path="/login" element={<Login />} />
       <Route
         path="/miembros"
         element={isAuthenticated ? <Miembros /> : <Navigate to="/login" />}
       />
+      <Route path="/chat" element={<ChatFamiliar />} />
+      <Route path="/chat-admin" element={<ChatAdmin />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
